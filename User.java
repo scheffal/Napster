e238873d529ab525a282e3d18e4fded8aaf6f2e7
scheffal <scheffal@mail.gvsu.edu>
2018-11-10 20:31:51 -0500

    Forgot to add User class before

---
+++ User.java
@@ -0,0 +1,15 @@
+public class User{
+
+    protected String userName;
+    protected String hostName;
+    protected String speed;
+    
+    public User(String userName, String hostName, String speed)
+    {
+	this.userName = userName;
+	this.hostName = hostName;
+	this.speed = speed;
+	
+    }
+
+}
